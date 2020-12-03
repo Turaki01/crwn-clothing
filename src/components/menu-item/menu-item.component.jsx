4f4eadd30a00c08ff3b165ceb85aae0d328bbe97
Turaki01 <ezekiel_zakari@yahoo.com>
2020-12-03 22:49:12 +0100

    menu item hover effect

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,9 +7,13 @@ const MenuItem = ({
     size
 }) => {
     return (
-        <div style={{
-                backgroundImage: `url(${imageUrl})`
-        }} className={`${size} menu-item`}>
+        <div className={`${size} menu-item`}>
+            <div
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+                className="background-image"
+            />
             <div className='content'>
                 <h1 className='title'>{title}</h1>
                 <span className='subtitle'>SHOP NOW</span>
