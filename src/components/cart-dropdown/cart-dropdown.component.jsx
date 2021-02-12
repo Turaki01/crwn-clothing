e9e6e881603d76d00f48d363a837d3982c08938e
Turaki01 <ezekiel_zakari@yahoo.com>
2021-02-12 07:35:40 +0100

    shop reducer and collection reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { selectCartItems, selectCartItemsCount } from '../../redux/cart/cart.selector'
+import { selectCartItems } from '../../redux/cart/cart.selector'
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component'
 import { createStructuredSelector } from 'reselect'
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import SHOP_DATA from './shop.data.js';
+import { createStructuredSelector } from 'reselect'
+
+import { selectCollections } from '../../redux/shop/shop.selector'
 
 import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+const ShopPage = ({collections}) => {
 
-  render() {
-    const { collections } = this.state;
     return (
       <div className='shop-page'>
         {collections.map(({ id, ...otherCollectionProps }) => (
@@ -22,7 +17,10 @@ class ShopPage extends React.Component {
         ))}
       </div>
     );
-  }
 }
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage);
